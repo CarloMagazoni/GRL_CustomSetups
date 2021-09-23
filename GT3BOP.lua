67e1309f202fec7b51a32479ed654746061da6c7
CarloMagazoni <58231398+CarloMagazoni@users.noreply.github.com>
2021-09-23 23:39:50 +0300

    Added GTE and GT3 files

---
+++ GT3BOP.lua
@@ -0,0 +1,204 @@
+--ApplyGT3Mod(GT3,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF)
+if CarNameCurrent=='PARAGON' then
+	 local Mass = 400
+	 local Drag = -0.000042
+	 local FWD = -1
+	 local RWD = 0
+	 local UpShift = -0.1
+	 local DownShift = -0.1
+	 local Drive = 0.03
+	 local MaxFlat = -1.234
+	 local InitMaxFlat = -1.0286
+	 local Brake = -0.2
+	 local Steering = -0.1
+	 local CurveMax = -0.245
+	 local CurveMaxR = 0.0377
+	 local CurveMin = -0.26
+	 local CurveMinR = -0.6945
+	 local CurveLat = 0
+	 local CurveLatR = 0.004
+	 local Coll=10
+	 local Eng=6
+	 local DF=0
+     ApplyGT3Mod(GT3,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF)
+  elseif CarNameCurrent=='SPECTER' then
+	 local Mass = 650
+	 local Drag = 0.000015
+	 local FWD = 0
+	 local RWD = 0
+	 local UpShift = -0.0999
+	 local DownShift = -0.0999
+	 local Drive = 0.02
+	 local MaxFlat = 0
+	 local InitMaxFlat = 0
+	 local Brake = -0.1
+	 local Steering = -0.1
+	 local CurveMax = 0
+	 local CurveMaxR = 0
+	 local CurveMin = 0
+	 local CurveMinR = 0
+	 local CurveLat = 0
+	 local CurveLatR = 0
+	 local Coll=10
+	 local Eng=10
+	 local DF=0
+     ApplyGT3Mod(GT3,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF)
+  elseif CarNameCurrent=='COQUETTE' then
+	 local Mass = 0
+	 local Drag = 0
+	 local FWD = 0
+	 local RWD = 0
+	 local UpShift = 0.8
+	 local DownShift = 0
+	 local Drive = 0.036
+	 local MaxFlat = 1.606
+	 local InitMaxFlat = 1.188
+	 local Brake = 0
+	 local Steering = 0
+	 local CurveMax = -0.12
+	 local CurveMaxR = 0.0194
+	 local CurveMin = -0.2
+	 local CurveMinR = -4.4445
+	 local CurveLat = 0
+	 local CurveLatR = 0
+	 local Coll=10
+	 local Eng=10
+	 local DF=0
+     ApplyGT3Mod(GT3,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF)
+  elseif CarNameCurrent=='ITALIRSX' then
+	 local Mass = -600
+	 local Drag = 0
+	 local FWD = -1
+	 local RWD = 0
+	 local UpShift = -5
+	 local DownShift = -5
+	 local Drive = -0.0492
+	 local MaxFlat = -1.723
+	 local InitMaxFlat = -1.4394
+	 local Brake = -0.4
+	 local Steering = -0.1
+	 local CurveMax = -0.067
+	 local CurveMaxR = 0.0268
+	 local CurveMin = -0.1
+	 local CurveMinR = -2.1667
+	 local CurveLat = -0.0262
+	 local CurveLatR = 0.1367
+	 local Coll=10
+	 local Eng=10
+	 local DF=-350
+     ApplyGT3Mod(GT3,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF)
+  elseif CarNameCurrent=='ELEGY2' then
+	 local Mass = 0
+	 local Drag = 0
+	 local FWD = -FWDCurrent
+	 local RWD = -0.399
+	 local UpShift = 0.8
+	 local DownShift = 0.8
+	 local Drive = 0
+	 local MaxFlat = 2
+	 local InitMaxFlat = 1.66
+	 local Brake = 0
+	 local Steering = 0
+	 local CurveMax = -0.15
+	 local CurveMaxR = 0.0218
+	 local CurveMin = -0.25
+	 local CurveMinR = -1.1667
+	 local CurveLat = 0
+	 local CurveLatR = -0.0225
+	 local Coll=10
+	 local Eng=10
+	 local DF=0
+     ApplyGT3Mod(GT3,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF)
+  elseif CarNameCurrent=='DRAFTER' then
+	 local Mass = 0
+	 local Drag = 0
+	 local FWD = -FWDCurrent
+	 local RWD = -0.199
+	 local UpShift = -0.1
+	 local DownShift = -0.1
+	 local Drive = 0.022
+	 local MaxFlat = 2.066
+	 local InitMaxFlat = 2
+	 local Brake = 0
+	 local Steering = 0
+	 local CurveMax = -0.12
+	 local CurveMaxR = 0.0218
+	 local CurveMin = 0.1
+	 local CurveMinR = -0.8267
+	 local CurveLat = 0
+	 local CurveLatR = 0
+	 local Coll=15
+	 local Eng=15
+	 local DF=0
+     ApplyGT3Mod(GT3,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF)
+  elseif CarNameCurrent=='SCHLAGEN' then
+	 local Mass = 400
+	 local Drag = 0.00010
+	 local FWD = 0
+	 local RWD = 0
+	 local UpShift = -3.2
+	 local DownShift = -3.5
+	 local Drive = -0.016
+	 local MaxFlat = -1
+	 local InitMaxFlat = -0.832
+	 local Brake = 0
+	 local Steering = -0.1
+	 local CurveMax = -0.07
+	 local CurveMaxR = 0.0105
+	 local CurveMin = -0.04
+	 local CurveMinR = 0.5555
+	 local CurveLat = 0
+	 local CurveLatR = 0
+	 local Coll=10
+	 local Eng=10
+	 local DF=-150
+     ApplyGT3Mod(GT3,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF)
+  elseif CarNameCurrent=='VECTRE' then
+	 local Mass = 0
+	 local Drag = 0
+	 local FWD = -FWDCurrent
+	 local RWD = -0.399
+	 local UpShift = -4.8
+	 local DownShift = -4.8
+	 local Drive = -0.024
+	 local MaxFlat = 3.11
+	 local InitMaxFlat = 2.63
+	 local Brake = 0
+	 local Steering = 0
+	 local CurveMax = -0.1
+	 local CurveMaxR = 0
+	 local CurveMin = 0
+	 local CurveMinR = 0
+	 local CurveLat = 0
+	 local CurveLatR = 0
+	 local Coll=10
+	 local Eng=10
+	 local DF=0
+     ApplyGT3Mod(GT3,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF)
+  elseif CarNameCurrent=='GROWLER' then
+	 local Mass = 0
+	 local Drag = 0
+	 local FWD = 0
+	 local RWD = 0
+	 local UpShift = -1.6
+	 local DownShift = -1.6
+	 local Drive = -0.0037
+	 local MaxFlat = 0.7827
+	 local InitMaxFlat = 0.3189
+	 local Brake = 0
+	 local Steering = 0
+	 local CurveMax = -0.03
+	 local CurveMaxR = 0.0119
+	 local CurveMin = -0.1449
+	 local CurveMinR = -1.8518
+	 local CurveLat = -0.06
+	 local CurveLatR = -0.1
+	 local Coll=10
+	 local Eng=10
+	 local DF=0
+     ApplyGT3Mod(GT3,0,0,0,0,-1.6,-1.6,-0.0037,0.7827,0.3189,0,0,-0.03,0.0119,-0.1449,-1.8518,-0.06,0,10,10,0)
+end
+
+--[LOG: Brakes for all cars -0.4]
+--[LOG: Brakes for all cars +0.4]
+--[LOG: Added Vectre and Growler]
---
+++ GTE.lua
@@ -0,0 +1,117 @@
+--ApplyGT3Mod(GT3,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF)
+if CarNameCurrent=='SPECTER2' then
+	 local Mass = 550
+	 local Drag = 0
+	 local FWD = 0
+	 local RWD = 0
+	 local UpShift = -1
+	 local DownShift = -1
+	 local Drive = 0.044
+	 local MaxFlat = 0.3327
+	 local InitMaxFlat = 2.2769
+	 local Brake = -0.2
+	 local Steering = -0.1
+	 local CurveMax = -0.24
+	 local CurveMaxR = 0.037
+	 local CurveMin = -0.3
+	 local CurveMinR = -2.7745
+	 local CurveLat = 0
+	 local CurveLatR = 0
+	 local Coll=10
+	 local Eng=10
+	 local DF=-1.75
+     ApplyGT3Mod(GT3,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF)
+ elseif CarNameCurrent=='COQUETTE4' then
+	 local Mass = -600
+	 local Drag = 0
+	 local FWD = -1
+	 local RWD = 0
+	 local UpShift = -5
+	 local DownShift = -5
+	 local Drive = -0.0492
+	 local MaxFlat = -1.723
+	 local InitMaxFlat = -1.4394
+	 local Brake = -0.4
+	 local Steering = -0.1
+	 local CurveMax = -0.067
+	 local CurveMaxR = 0.0268
+	 local CurveMin = -0.1
+	 local CurveMinR = -2.1667
+	 local CurveLat = -0.0262
+	 local CurveLatR = 0.1367
+	 local Coll=10
+	 local Eng=10
+	 local DF=-350
+     ApplyGT3Mod(GT3,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF)
+ elseif CarNameCurrent=='ITALIGTO' then
+	 local Mass = -600
+	 local Drag = 0
+	 local FWD = -1
+	 local RWD = 0
+	 local UpShift = -5
+	 local DownShift = -5
+	 local Drive = -0.0492
+	 local MaxFlat = -1.723
+	 local InitMaxFlat = -1.4394
+	 local Brake = -0.4
+	 local Steering = -0.1
+	 local CurveMax = -0.067
+	 local CurveMaxR = 0.0268
+	 local CurveMin = -0.1
+	 local CurveMinR = -2.1667
+	 local CurveLat = -0.0262
+	 local CurveLatR = 0.1367
+	 local Coll=10
+	 local Eng=10
+	 local DF=-350
+     ApplyGT3Mod(GT3,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF)
+ elseif CarNameCurrent=='comets2' then
+	 local Mass = -600
+	 local Drag = 0
+	 local FWD = -1
+	 local RWD = 0
+	 local UpShift = -5
+	 local DownShift = -5
+	 local Drive = -0.0492
+	 local MaxFlat = -1.723
+	 local InitMaxFlat = -1.4394
+	 local Brake = -0.4
+	 local Steering = -0.1
+	 local CurveMax = -0.067
+	 local CurveMaxR = 0.0268
+	 local CurveMin = -0.1
+	 local CurveMinR = -2.1667
+	 local CurveLat = -0.0262
+	 local CurveLatR = 0.1367
+	 local Coll=10
+	 local Eng=10
+	 local DF=-350
+     ApplyGT3Mod(GT3,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF)
+ elseif CarNameCurrent=='CYPHER' then
+	 local Mass = -600
+	 local Drag = 0
+	 local FWD = -1
+	 local RWD = 0
+	 local UpShift = -5
+	 local DownShift = -5
+	 local Drive = -0.0492
+	 local MaxFlat = -1.723
+	 local InitMaxFlat = -1.4394
+	 local Brake = -0.4
+	 local Steering = -0.1
+	 local CurveMax = -0.067
+	 local CurveMaxR = 0.0268
+	 local CurveMin = -0.1
+	 local CurveMinR = -2.1667
+	 local CurveLat = -0.0262
+	 local CurveLatR = 0.1367
+	 local Coll=10
+	 local Eng=10
+	 local DF=-350
+     ApplyGT3Mod(GT3,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF)
+
+end
+
+--[LOG: Brakes for all cars -0.4]
+--[LOG: Brakes for all cars +0.4]
+--[LOG: Added Vectre and Growler]
