a5da29566deda2421d55cdb86178dc9ee483f8fb
CarloMagazoni <58231398+CarloMagazoni@users.noreply.github.com>
2021-09-25 03:41:55 +0300

    Create HS.lua

---
+++ HS.lua
@@ -0,0 +1,159 @@
+--ApplyHSMod(HS,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF,Flag,Front,Rear,Inertia)
+if CarNameCurrent=='VAGNER' then
+	 local Mass = 0
+	 local Drag = 0
+	 local FWD = 0
+	 local RWD = 0
+	 local UpShift = 0
+	 local DownShift = 0
+	 local Drive = 0
+	 local MaxFlat = 0
+	 local InitMaxFlat = 0
+	 local Brake = 0
+	 local Steering = 0
+	 local CurveMax = 0
+	 local CurveMaxR = 0
+	 local CurveMin = 0
+	 local CurveMinR = 0
+	 local CurveLat = 0
+	 local CurveLatR = 0 
+	 local Coll = 10
+	 local Eng = 10
+	 local DF = 0
+	 local Flag = 4
+	 local Front = 0
+	 local Rear = 0
+	 local Inertia = 0
+     ApplyHSMod(HS,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF,Flag,Front,Rear,Inertia)
+ elseif CarNameCurrent=='KRIEGER' then
+	 local Mass = 0
+	 local Drag = 0
+	 local FWD = 0
+	 local RWD = 0
+	 local UpShift = 0
+	 local DownShift = 0
+	 local Drive = 0
+	 local MaxFlat = 0
+	 local InitMaxFlat = 0
+	 local Brake = 0
+	 local Steering = 0
+	 local CurveMax = 0
+	 local CurveMaxR = 0
+	 local CurveMin = 0
+	 local CurveMinR = 0
+	 local CurveLat = 0
+	 local CurveLatR = 0 
+	 local Coll = 10
+	 local Eng = 10
+	 local DF = 0
+	 local Flag = 4
+	 local Front = 0
+	 local Rear = 0
+	 local Inertia = 0
+     ApplyHSMod(HS,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF,Flag,Front,Rear,Inertia)
+ elseif CarNameCurrent=='VISIONE' then
+	 local Mass = 0
+	 local Drag = 0
+	 local FWD = 0
+	 local RWD = 0
+	 local UpShift = 0
+	 local DownShift = 0
+	 local Drive = 0
+	 local MaxFlat = 0
+	 local InitMaxFlat = 0
+	 local Brake = 0
+	 local Steering = 0
+	 local CurveMax = 0
+	 local CurveMaxR = 0
+	 local CurveMin = 0
+	 local CurveMinR = 0
+	 local CurveLat = 0
+	 local CurveLatR = 0 
+	 local Coll = 10
+	 local Eng = 10
+	 local DF = 0
+	 local Flag = 4
+	 local Front = 0
+	 local Rear = 0
+	 local Inertia = 0
+     ApplyHSMod(HS,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF,Flag,Front,Rear,Inertia)
+ elseif CarNameCurrent=='EMERUS' then
+	 local Mass = 500
+	 local Drag = 0.0002
+	 local FWD = 0
+	 local RWD = 0
+	 local UpShift = 0
+	 local DownShift = 0
+	 local Drive = 0.6
+	 local MaxFlat = 6
+	 local InitMaxFlat = 10
+	 local Brake = -0.4
+	 local Steering = 0
+	 local CurveMax = -0.1
+	 local CurveMaxR = 0
+	 local CurveMin = 0
+	 local CurveMinR = 0
+	 local CurveLat = -0.1
+	 local CurveLatR = 0 
+	 local Coll = 10
+	 local Eng = 10
+	 local DF = 0
+	 local Flag = 4
+	 local Front = -0.06
+	 local Rear = 0.06
+	 local Inertia = -0.35
+     ApplyHSMod(HS,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF,Flag,Front,Rear,Inertia)
+ elseif CarNameCurrent=='TIGON' then
+	 local Mass = 0
+	 local Drag = 0
+	 local FWD = 0
+	 local RWD = 0
+	 local UpShift = 0
+	 local DownShift = 0
+	 local Drive = 0
+	 local MaxFlat = 0
+	 local InitMaxFlat = 0
+	 local Brake = 0
+	 local Steering = 0
+	 local CurveMax = 0
+	 local CurveMaxR = 0
+	 local CurveMin = 0
+	 local CurveMinR = 0
+	 local CurveLat = 0
+	 local CurveLatR = 0 
+	 local Coll = 10
+	 local Eng = 10
+	 local DF = 0
+	 local Flag = 4
+	 local Front = 0
+	 local Rear = 0
+	 local Inertia = 0
+     ApplyHSMod(HS,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF,Flag,Front,Rear,Inertia)
+ elseif CarNameCurrent=='TYRANT' then
+	 local Mass = 0
+	 local Drag = 0
+	 local FWD = 0
+	 local RWD = 0
+	 local UpShift = 0
+	 local DownShift = 0
+	 local Drive = 0
+	 local MaxFlat = 0
+	 local InitMaxFlat = 0
+	 local Brake = 0
+	 local Steering = 0
+	 local CurveMax = 0
+	 local CurveMaxR = 0
+	 local CurveMin = 0
+	 local CurveMinR = 0
+	 local CurveLat = 0
+	 local CurveLatR = 0 
+	 local Coll = 10
+	 local Eng = 10
+	 local DF = 0
+	 local Flag = 4
+	 local Front = 0
+	 local Rear = 0
+	 local Inertia = 0
+     ApplyHSMod(HS,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF,Flag,Front,Rear,Inertia)
+
+end
