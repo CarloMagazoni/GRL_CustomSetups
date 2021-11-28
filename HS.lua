bccacf80b89aaafde46e6943630f440da1687881
CarloMagazoni <58231398+CarloMagazoni@users.noreply.github.com>
2021-11-29 01:15:09 +0300

    Delete Tyrant

--- HS.lua
+++ HS.lua
@@ -16,7 +16,7 @@ if CarNameCurrent=='VAGNER' then
 	 local CurveMin = 0.05
 	 local CurveMinR = 0
 	 local CurveLat = 0.09
-	 local CurveLatR = 0 
+	 local CurveLatR = 0
 	 local Coll = 10
 	 local Eng = 10
 	 local DF = 0
@@ -24,9 +24,11 @@ if CarNameCurrent=='VAGNER' then
 	 local Front = 0
 	 local Rear = 0
 	 local Inertia = 0
-	 local Model = 128
+	 local Model = 0
 	 local ADV = 4194304
 	 local Lift = 0
+	 --128
+	 --4194304
      ApplyHSMod(HS,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF,Flag,Front,Rear,Inertia,ADV,Model,Lift)
  elseif CarNameCurrent=='krieger' then
 	 local Mass = 0
@@ -45,7 +47,7 @@ if CarNameCurrent=='VAGNER' then
 	 local CurveMin = -0.05
 	 local CurveMinR = 0
 	 local CurveLat = -0.02
-	 local CurveLatR = 0 
+	 local CurveLatR = 0
 	 local Coll = 10
 	 local Eng = 10
 	 local DF = 0
@@ -53,7 +55,7 @@ if CarNameCurrent=='VAGNER' then
 	 local Front = 0.02
 	 local Rear = -0.02
 	 local Inertia = -0.45
-	 local Model = 128
+	 local Model = 0
 	 local ADV = 4194304
 	 local Lift = 0
      ApplyHSMod(HS,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF,Flag,Front,Rear,Inertia,ADV,Model,Lift)
@@ -74,7 +76,7 @@ if CarNameCurrent=='VAGNER' then
 	 local CurveMin = 0.04
 	 local CurveMinR = 0
 	 local CurveLat = -0.01
-	 local CurveLatR = 0 
+	 local CurveLatR = 0
 	 local Coll = 10
 	 local Eng = 10
 	 local DF = 0
@@ -82,7 +84,7 @@ if CarNameCurrent=='VAGNER' then
 	 local Front = 0.04
 	 local Rear = 0.02
 	 local Inertia = 0
-	 local Model = 128
+	 local Model = 0
 	 local ADV = 4194304
 	 local Lift = 0
      ApplyHSMod(HS,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF,Flag,Front,Rear,Inertia,ADV,Model,Lift)
@@ -103,7 +105,7 @@ if CarNameCurrent=='VAGNER' then
 	 local CurveMin = 0
 	 local CurveMinR = 0
 	 local CurveLat = -0.1
-	 local CurveLatR = 0 
+	 local CurveLatR = 0
 	 local Coll = 10
 	 local Eng = 10
 	 local DF = 0
@@ -111,7 +113,7 @@ if CarNameCurrent=='VAGNER' then
 	 local Front = -0.05
 	 local Rear = 0.05
 	 local Inertia = -0.35
-	 local Model = 128
+	 local Model = 0
 	 local ADV = 4194304
 	 local Lift = 0
      ApplyHSMod(HS,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF,Flag,Front,Rear,Inertia,ADV,Model,Lift)
@@ -132,7 +134,7 @@ if CarNameCurrent=='VAGNER' then
 	 local CurveMin = 0
 	 local CurveMinR = 0
 	 local CurveLat = 0.1
-	 local CurveLatR = 0 
+	 local CurveLatR = 0
 	 local Coll = 10
 	 local Eng = 10
 	 local DF = 0
@@ -140,37 +142,8 @@ if CarNameCurrent=='VAGNER' then
 	 local Front = 0.06
 	 local Rear = 0
 	 local Inertia = -0.35
-	 local Model = 128
+	 local Model = 0
 	 local ADV = 4194304
 	 local Lift = 0
-     ApplyHSMod(HS,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF,Flag,Front,Rear,Inertia,ADV,Model,Lift)
- elseif CarNameCurrent=='TYRANT' then
-	 local Mass = 0
-	 local Drag = 0
-	 local FWD = 0
-	 local RWD = 0
-	 local UpShift = 3
-	 local DownShift = 3
-	 local Drive = 0.52
-	 local MaxFlat = 2.5
-	 local InitMaxFlat = 10
-	 local Brake = 0
-	 local Steering = -0.05
-	 local CurveMax = -0.05
-	 local CurveMaxR = 0
-	 local CurveMin = -0.1
-	 local CurveMinR = 0
-	 local CurveLat = 0
-	 local CurveLatR = 0 
-	 local Coll = 10
-	 local Eng = 10
-	 local DF = 0
-	 local Flag = 0
-	 local Front = -0.015
-	 local Rear = 0.05
-	 local Inertia = 0
-	 local Model = 128
-	 local ADV = 4194304
-	 local Lift = 0.05
      ApplyHSMod(HS,Mass,Drag,FWD,RWD,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,Steering,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Coll,Eng,DF,Flag,Front,Rear,Inertia,ADV,Model,Lift)
 end
