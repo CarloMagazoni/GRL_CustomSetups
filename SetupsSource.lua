8028eb40135bf160441c1a8baccf053ed67d7d75
Alexey Zagorodnev <58231398+CarloMagazoni@users.noreply.github.com>
2023-04-28 02:40:34 +0300

    Update SetupsSource.lua

--- SetupsSource.lua
+++ SetupsSource.lua
@@ -123,7 +123,7 @@
         HWID_Array[1]["HWID"] = getDataFromURL(http, "E", id, "str") --HWID
         Name = HWID_Array[1]["NAME"]
         DBID = HWID_Array[1]
-        Username ="User: "..HWID_Array[i]["NAME"]
+        Username ="User: "..HWID_Array[1]["NAME"]
         SendPack("Launched App",1 ,1)
         NewUser=false
         break
