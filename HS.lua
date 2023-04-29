0ce3cfe87cceedf0371e23d4874bd8b311940b1f
Alexey Zagorodnev <58231398+CarloMagazoni@users.noreply.github.com>
2023-04-29 22:39:34 +0300

    Update HS.lua

--- HS.lua
+++ HS.lua
@@ -33,8 +33,8 @@ if CarNameCurrent=='VAGNER' then
  elseif CarNameCurrent=='IGNUS' then
 	 local Mass = 0
 	 local Drag = 0
-	 local FWD = -0.5
-	 local RWD = 0.5
+	 local FWD = -0.50
+	 local RWD = 0.50
 	 local UpShift = 0.2
 	 local DownShift = -1.0
 	 local Drive = 0.36
