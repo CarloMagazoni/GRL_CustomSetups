f7c6eea74efcbaca604cc63587b8525fb4908c06
Alexey Zagorodnev <58231398+CarloMagazoni@users.noreply.github.com>
2023-06-27 19:42:05 +0300

    Update SetupValues.lua

--- SetupValues.lua
+++ SetupValues.lua
@@ -46,7 +46,7 @@ elseif CarNameCurrent=='EMERUS' then
     brakeSizeDragVAL = 0.000015
     bbfrontVAL = 0.055
     bbrearVAL = 0.055
-    gearsInertiaVAL = 0.25
+    gearsInertiaVAL = 0.15
     gearsMaxFlatVAL = 6
     gearsInitMaxFlatVAL = 6
     mixRWDVAL = 0.00000045
@@ -178,9 +178,9 @@ elseif CarNameCurrent=='krieger' then
     brakeSizeDragVAL = 0.000015
     bbfrontVAL = 0.055
     bbrearVAL = 0.055
-    gearsInertiaVAL = 0.25
-    gearsMaxFlatVAL = 6
-    gearsInitMaxFlatVAL = 6
+    gearsInertiaVAL = 0.15
+    gearsMaxFlatVAL = 5
+    gearsInitMaxFlatVAL = 5
     mixRWDVAL = 0.00000045
     mixMixtureVAL = 0.06
     susForceVAL = 0.05
