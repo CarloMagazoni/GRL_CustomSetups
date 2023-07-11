6369c0fcf088046544257c4c7296cd7415f215f4
Alexey Zagorodnev <58231398+CarloMagazoni@users.noreply.github.com>
2023-07-11 15:39:33 +0300

    version update

--- SetupsSource.lua
+++ SetupsSource.lua
@@ -356,7 +356,7 @@
          elseif pic == 2 then UDF1.Pic4.Visible=true
         end
 
-        UDF1.Caption="Custom Setups | Online v1.9.4"
+        UDF1.Caption="Custom Setups | Online v1.9.5"
         --UDF1.UserNameLabel.Alignment = "taRightJustify"
         local SpaceBars = ""
         UDF1.UserNameLabel.Caption = Name..SpaceBars
