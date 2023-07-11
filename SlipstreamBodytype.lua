7dccaf266a5e00361c4c6697f4014ef7e52c376c
Alexey Zagorodnev <58231398+CarloMagazoni@users.noreply.github.com>
2023-07-11 15:27:57 +0300

    Create SlipstreamBodytype.lua

---
+++ SlipstreamBodytype.lua
@@ -0,0 +1,5 @@
+if CarNameCurrent=='VISIONE' or CarNameCurrent=='EMERUS' or CarNameCurrent=='krieger' or CarNameCurrent=='VAGNER' then
+    slipBodyTypeForce = 0.25
+elseif CarNameCurrent=='OPENWHEEL1' then
+    slipBodyTypeForce = 0.35
+end
