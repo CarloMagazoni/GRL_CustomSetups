83d6abfc3015df365cf3889e5ab2bffcb353b96a
Alexey Zagorodnev <58231398+CarloMagazoni@users.noreply.github.com>
2023-07-24 19:05:54 +0300

    Update DRIFT.lua

--- DRIFT.lua
+++ DRIFT.lua
@@ -1,22 +1,32 @@
 --ApplyDRIFTMod(IntoDRIFT,FWD,RWD,Drive,MaxFlat,Brake,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Steering)
 if CarNameCurrent=='ELEGY' then
-	 local FWD = -0.5
-	 local RWD = -0.5
-	 local Drive = 0.6
-	 local MaxFlat = 0
-	 local Steering = 0.2
-	 local CurveMax = -1.8 --2.67
-	 local CurveMin = -1.0 --2.55
-	 local CurveLat = 2.5 --0.3926
-	 ApplyDRIFTMod(DRIFT,FWD,RWD,Drive,MaxFlat,CurveMax,CurveMin,CurveLat,Steering)
-elseif CarNameCurrent=='CYPHER' then
-	 local FWD = 0
-	 local RWD = 0
-	 local Drive = 0.6
-	 local MaxFlat = 0
-	 local Steering = 0.2
-	 local CurveMax = -1.8 --2.67
-	 local CurveMin = -1.0 --2.55
-	 local CurveLat = 2.5 --0.3926
-	 ApplyDRIFTMod(DRIFT,FWD,RWD,Drive,MaxFlat,CurveMax,CurveMin,CurveLat,Steering)
+	local FWD = -0.5
+	local RWD = -0.5
+	local Drive = 0.6
+	local MaxFlat = 0
+	local Steering = 0.25
+	local CurveMax = -1.8 --2.67
+	local CurveMin = -1.0 --2.55
+	local CurveLat = 2.5 --0.3926
+	ApplyDRIFTMod(DRIFT,FWD,RWD,Drive,MaxFlat,CurveMax,CurveMin,CurveLat,Steering)
+elseif CarNameCurrent=='RT3000' then
+	local FWD = 0
+	local RWD = 0
+	local Drive = 0.6
+	local MaxFlat = 0
+	local Steering = 0.25
+	local CurveMax = -1.8 --2.67
+	local CurveMin = -1.0 --2.55
+	local CurveLat = 2.5 --0.3926
+	ApplyDRIFTMod(DRIFT,FWD,RWD,Drive,MaxFlat,CurveMax,CurveMin,CurveLat,Steering)
+elseif CarNameCurrent=='EUROS' then
+	local FWD = 0
+	local RWD = 0
+	local Drive = 0.6
+	local MaxFlat = 0
+	local Steering = 0.25
+	local CurveMax = -1.8 --2.67
+	local CurveMin = -1.0 --2.55
+	local CurveLat = 2.5 --0.3926
+	ApplyDRIFTMod(DRIFT,FWD,RWD,Drive,MaxFlat,CurveMax,CurveMin,CurveLat,Steering)
 end
