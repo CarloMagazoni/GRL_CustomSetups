1ae68ce7f393d4a5ddf99bbd2d7970569a81485d
Alexey Zagorodnev <58231398+CarloMagazoni@users.noreply.github.com>
2023-07-25 13:09:18 +0300

    Drift

--- DRIFT.lua
+++ DRIFT.lua
@@ -54,7 +54,7 @@ elseif CarNameCurrent=='SCHWARTZER' then
 	local CurveLat = 1 --0.3926
 	local CurveLatR = -0.5
 	ApplyDRIFTMod(DRIFT,FWD,RWD,Drive,MaxFlat,CurveMax,CurveMin,CurveLat,Steering,CurveLatR)
-elseif CarNameCurrent=='JESTERRR' then
+elseif CarNameCurrent=='JESTER4' then
 	local FWD = 0
 	local RWD = 0
 	local Drive = 0.6
