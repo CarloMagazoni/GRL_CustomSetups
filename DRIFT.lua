913dd563b4f0237342ffe1bdf4204ef12cc58a95
Alexey Zagorodnev <58231398+CarloMagazoni@users.noreply.github.com>
2023-07-27 23:16:55 +0300

    Update DRIFT.lua

--- DRIFT.lua
+++ DRIFT.lua
@@ -88,13 +88,13 @@ elseif CarNameCurrent=='ZR350' then
 	local CurveLatR = -0.5
 	ApplyDRIFTMod(DRIFT,FWD,RWD,Drive,MaxFlat,CurveMax,CurveMin,CurveLat,Steering,CurveLatR)
 elseif CarNameCurrent=='ELEGY2' then
-	local FWD = -0.5
-	local RWD = -0.5
+	local FWD = -0.6
+	local RWD = -0.4
 	local Drive = 0.6
 	local MaxFlat = 0
-	local Steering = 0.25
-	local CurveMax = -1.8 --2.67
-	local CurveMin = -0.9 --2.55
+	local Steering = 0.4
+	local CurveMax = -1.85 --2.67
+	local CurveMin = -1.0 --2.55
 	local CurveLat = 1 --0.3926
 	local CurveLatR = -0.5
 	ApplyDRIFTMod(DRIFT,FWD,RWD,Drive,MaxFlat,CurveMax,CurveMin,CurveLat,Steering,CurveLatR)
