123cd0b1b095761712346f6fd1eb5be7f5c3b3e5
Alexey Zagorodnev <58231398+CarloMagazoni@users.noreply.github.com>
2023-08-01 15:02:52 +0300

    Update SetupValues.lua

--- SetupValues.lua
+++ SetupValues.lua
@@ -228,7 +228,7 @@ elseif CarNameCurrent=='OPENWHEEL1' then
     susRearARBVAL = 0.175
     susRearRoloVAL = 0.025
 else
-    steeringLockVAL = 0.005
+    steeringLockVAL = 0.006
     steeringLockRatioVAL = 0.01
     rearWingDragVAL = 0.0000795
     rearWingGripVAL = 0.01575
