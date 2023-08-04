b7082ff163fb005315f03ac43b0c4e38640b328c
Alexey Zagorodnev <58231398+CarloMagazoni@users.noreply.github.com>
2023-08-04 08:21:10 +0300

    Update DRIFT.lua

--- DRIFT.lua
+++ DRIFT.lua
@@ -30,14 +30,14 @@ elseif CarNameCurrent=='RT3000' then
 elseif CarNameCurrent=='EUROS' then
 	local FWD = 0
 	local RWD = 0
-	local Drive = 0.6
+	local Drive = 0.62
 	local MaxFlat = 0
 	local Steering = 0.285
 	local CurveMax = -1.8 --2.67
 	local CurveMin = -0.8 --2.55
 	local CurveLat = 1 --0.3926
 	local CurveLatR = -0.5
-	local LowSpeed = 0
+	local LowSpeed = -0.1
 	local Mass = 2000
 	local Handbrake = 0.5
 	ApplyDRIFTMod(DRIFT,FWD,RWD,Drive,MaxFlat,CurveMax,CurveMin,CurveLat,Steering,CurveLatR,LowSpeed,Mass,Handbrake)
@@ -134,7 +134,7 @@ elseif CarNameCurrent=='REMUS' then
 	local CurveMin = -1.0 --2.55
 	local CurveLat = 1 --0.3926
 	local CurveLatR = -0.5
-	local LowSpeed = -0.2
+	local LowSpeed = -0.3
 	local Mass = 2000
 	ApplyDRIFTMod(DRIFT,FWD,RWD,Drive,MaxFlat,CurveMax,CurveMin,CurveLat,Steering,CurveLatR,LowSpeed,Mass,Handbrake)
 elseif CarNameCurrent=='NEON' then
@@ -170,12 +170,12 @@ elseif CarNameCurrent=='R300' then
 	local RWD = 0
 	local Drive = 0.6
 	local MaxFlat = 0
-	local Steering = 0.25
-	local CurveMax = -1.7 --2.67
-	local CurveMin = -0.8 --2.55
+	local Steering = 0.275
+	local CurveMax = -1.65 --2.67
+	local CurveMin = -0.75 --2.55
 	local CurveLat = 1 --0.3926
 	local CurveLatR = -0.5
-	local LowSpeed = 0
+	local LowSpeed = -0.1
 	local Mass = 2000
 	local Handbrake = 0.5
 	ApplyDRIFTMod(DRIFT,FWD,RWD,Drive,MaxFlat,CurveMax,CurveMin,CurveLat,Steering,CurveLatR,LowSpeed,Mass,Handbrake)
