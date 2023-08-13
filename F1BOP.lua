89b0d1dcc9b5d344931edf9381fce94966c3edb9
Alexey Zagorodnev <58231398+CarloMagazoni@users.noreply.github.com>
2023-08-13 20:30:43 +0300

    Update F1BOP.lua

--- F1BOP.lua
+++ F1BOP.lua
@@ -27,35 +27,6 @@ if CarNameCurrent=='FORMULA' then
 	local Rolo = 0
 	local Steering = 0
 	ApplyF1Mod(F1,Mass,Drag,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Front,Rear,Force,Bump,Travel,SuspensionLift,FS,RS,ARB,Rolo,DF,Steering)
-elseif CarNameCurrent=='OPENWHEEL1' and Username == 'User: v takt ' then
-	local Mass = 0
-	local Drag = 0
-	local UpShift = 0
-	local DownShift = 0
-	local Drive = 1
-	local MaxFlat = 0 
-	local InitMaxFlat = 0
-	local Brake = -0.78 
-	local CurveMax = 0
-	local CurveMaxR = 0
-	local CurveMin = 0
-	local CurveMinR = 0
-	local CurveLat = 0
-	local CurveLatR = 0
-	local DF = -1
-	local Front = 0
-	local Rear = 0
-	local SuspensionLift = 0
-	local Force = 0
-	local Bump = 0
-	local ARB = 0
-	local FS = 0
-	local RS = 0
-	local Travel = 0
-	local Rolo = 0
-	local Steering = -6
-	speakEnglish('Cars, Cars, Musorka')
-	ApplyF1Mod(F1,Mass,Drag,UpShift,DownShift,Drive,MaxFlat,InitMaxFlat,Brake,CurveMax,CurveMaxR,CurveMin,CurveMinR,CurveLat,CurveLatR,Front,Rear,Force,Bump,Travel,SuspensionLift,FS,RS,ARB,Rolo,DF,Steering)
 elseif CarNameCurrent=='OPENWHEEL1' then
 	local Mass = 0
 	local Drag = 0
